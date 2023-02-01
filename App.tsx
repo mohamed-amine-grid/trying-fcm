ddad5f73e66949c336f9f11d3fc7df2a397bed85
jakom <mohammed.amine-grid@solvr.fr>
2023-02-01 14:24:44 +0100

    clean App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,7 @@
 import { Text, View } from 'react-native';
-import { NativeWindStyleSheet } from "nativewind";
 
 export default function App() {
   return (
    <Text className='bg-red-700'>Hello</Text>
   );
 }
-
-NativeWindStyleSheet.setOutput({
-  default:'native'
-})
